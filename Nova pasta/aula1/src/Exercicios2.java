40021d57497d630c0e0cf3a2c611bdbf188e33a6
Maria Luíza Tomio <martomio@furb.br>
2022-05-02 20:47:28 -0300

    projeto final

---
+++ Nova pasta/aula1/src/Exercicios2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicios2 {
+    public static void main(String[] args) {
+        //Entrada
+        Scanner in = new Scanner(System.in);
+        System.out.println("Informe um valor: ");
+        int valor = (in.nextInt());
+
+        //Processamento
+        if (valor > 10){
+            System.out.println("É maior que 10!");
+        } else {
+            //Saída
+            System.out.println("Não é maior do que 10!");
+        }
+
+
+
+
+    }
+}
