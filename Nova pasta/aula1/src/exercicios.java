ff475929d5986785f206003b6831df2ab5cd1be2
unknown <malu.tomio2006@hotmail.com>
2022-05-12 17:51:15 -0300

    boneco-Forca-ADD

--- Nova pasta/aula1/src/exercicios.java
+++ Nova pasta/aula1/src/exercicios.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class exercicios {
     public static void main(String[] args) {
-        exe05();
+        exe20();
     }
     //exercise 1
     public static void exe01() {
@@ -22,7 +22,7 @@ public class exercicios {
         System.out.println("exercicio2");
         System.out.print("Digite um valor");
         int valor = in.nextInt();
-        System.out.println(--valor);
+        System.out.println("seu antecessor é " + (--valor));
 
     }
 
@@ -35,7 +35,8 @@ public class exercicios {
         double base = in.nextInt();
         System.out.println("a base é " + base);
         System.out.println("a área do retângulo é " + base * altura);
-        System.out.print("o perímetro é " + (base + base + altura + altura));
+        System.out.println("o perímetro é " + (base + base + altura + altura));
+        System.out.println();
     }
 
     public static void exe04() {
@@ -63,22 +64,49 @@ public class exercicios {
         System.out.println("Quantidade de votos validos");
         System.out.println(pessoa - (branco + nulo));
         System.out.print("A porcentagem de pessoas que votaram em branco é de ");
-        double var = (branco * 100.0) / pessoa;
-        System.out.printf(var + "%");
+        double var = (branco * 100) / pessoa;
+        System.out.println(var + "%");
         System.out.println();
         System.out.print("A porcentagem de pessoas que votaram nulo é de ");
-     double var2 = (nulo * 100.0) / pessoa;
-        System.out.printf(var2 + "%");
+        double var2 = (nulo * 100) / pessoa;
+        System.out.println(var2 + "%");
         System.out.println();
         System.out.println();
         System.out.print("A porcentagem de votos válidos é de " );
-double var3 = ((pessoa - (branco + nulo)) * 100.0) / pessoa;
-        System.out.printf(var3 2.f + "%");
+        double var3 = ((pessoa - (branco + nulo)) * 100) / pessoa;
+        System.out.println(var3 + "%");
     }
 
     public static void exe06() {
+        Scanner ler = new Scanner(System.in);
+double salario, reajuste, novoSalario;
+        System.out.printf("Informe o salário mensal:\n");
+        salario = ler.nextDouble();
+        System.out.printf("\nInforme o percentual de reajuste:\n");
+        reajuste = ler.nextDouble();
+        novoSalario = salario + (salario * (reajuste / 100));
+        System.out.printf("\n\nSalario novo = R$ %.2f\n", novoSalario);
+    }
+    public static void exe20() {
         Scanner in = new Scanner(System.in);
+        System.out.println("Digite seu salário fixo");
+        double salario = in.nextDouble();
 
+        System.out.println("Digite o valor das suas vendas efetuadas");
+        double vendas = in.nextDouble();
+
+        //salário abaixo de 1500
+        double
+                salarionormal = salario + (vendas * 0.03);
+        if (vendas <= 1500){
+            System.out.println("Seu salário final: " + salarionormal);
+        }
+        double salarioextra = (salarionormal + (0.05*(vendas - 1500)));
+        if (vendas > 1500)
+            System.out.println("Seu salário final: " + salarioextra);
+        // o erro que ta dando é que nn sabe qnd as vendas são maiores que 1500 pois dai no salário normal ele calcula como 0.03 * ...
     }
-    }
+}
+
+
 
